e0de649155575fad9a3731eada74dfacccf5702e
Kevin Burke <kev@inburke.com>
2014-11-17 12:09:42 -0800

    correctly use the random function to get random numbers

--- filestorer.go
+++ filestorer.go
@@ -11,15 +11,16 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 // via http://stackoverflow.com/a/22892986/329700
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-func randSeq(n int) string {
+func randSeq(rando *rand.Rand, n int) string {
 	b := make([]rune, n)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[rando.Intn(len(letters))]
 	}
 	return string(b)
 }
@@ -29,13 +30,14 @@ type response struct {
 	Success bool `json:"success"`
 }
 
-func metricsHandler(w http.ResponseWriter, r *http.Request, directory string) {
+func metricsHandler(w http.ResponseWriter, r *http.Request, directory string, rando *rand.Rand) {
 	if strings.ToUpper(r.Method) != "POST" {
 		http.Error(w, fmt.Sprintf("%s method not supported. Upload data via POST", r.Method), 405)
 		return
 	}
-	randFile := randSeq(10)
+	randFile := randSeq(rando, 10)
 	f, err := os.Create(filepath.Join(directory, randFile))
+	defer f.Close()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -62,6 +64,7 @@ func metricsHandler(w http.ResponseWriter, r *http.Request, directory string) {
 }
 
 func main() {
+	rando := rand.New(rand.NewSource(time.Now().UnixNano()))
 	directory := flag.String("directory", "", "Directory to store metrics data in")
 	flag.Parse()
 	if len(flag.Args()) != 0 || *directory == "" {
@@ -72,7 +75,7 @@ func main() {
 		log.Fatal(err.Error())
 	}
 	http.HandleFunc("/api/targets/v1/metrics", func(w http.ResponseWriter, r *http.Request) {
-		metricsHandler(w, r, *directory)
+		metricsHandler(w, r, *directory, rando)
 	})
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
